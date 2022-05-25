6724b7b1d3733a36386f37b13c9d3bd4016f6e6b
Golovnya Vitaliy <vitas210295@gmail.com>
2022-05-25 19:58:45 +1000

    Initial commit

---
+++ m2m-relations/articles/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=256, verbose_name='Название')
+    text = models.TextField(verbose_name='Текст')
+    published_at = models.DateTimeField(verbose_name='Дата публикации')
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+        ordering = ['-published_at']
+
+    def __str__(self):
+        return self.title
+
+
+class Scope(models.Model):
+    tag = models.ForeignKey(Tag, related_name='scopes', on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, related_name='scopes', on_delete=models.CASCADE)
+    is_main = models.BooleanField(default=False, verbose_name='Основной')
